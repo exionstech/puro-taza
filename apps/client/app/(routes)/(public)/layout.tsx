dbcf5655530ce410e0f5e2d7a47d8df17f4f91cc
gourab8389 <deyg6988@gmail.com>
2024-12-07 15:26:37 +0530

    feat: add Navbar and Logo components, update PublicRouteLayout to include Navbar, and remove unused SVG files

--- apps/client/app/(routes)/(public)/layout.tsx
+++ apps/client/app/(routes)/(public)/layout.tsx
@@ -1,8 +1,12 @@
+import Navbar from "@/components/shared/navbar";
 import React from "react";
 
 const PublicRouteLayout = ({ children }: PublicRouteLayoutProps) => {
   return (
-  <main>
+  <main className="flex flex-col items-center scroll-smooth min-h-screen">
+    <div className="">
+    <Navbar/>
+    </div>
     {children}
   </main>
   );
--- apps/client/components/shared/navbar.tsx
+++ apps/client/components/shared/navbar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Logo from './logo';
 
 const Navbar = () => {
   return (
     <div className='fixed right-0 left-0 top-0 w-full max-w-screen-2xl px-14 py-5 items-center md:flex hidden'>
-      <div className="w-[10%] items-center flex"></div>
+      <div className="w-[10%] items-center flex">
+        <Logo height={70} width={70}/>
+      </div>
       <div className="w-[70%] items-center flex"></div>
       <div className="w-[20%] items-center flex"></div>
     </div>

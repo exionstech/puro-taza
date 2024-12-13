5d8681d6bc86a95a1787dc18f67b068f28e8bd1f
gourab8389 <deyg6988@gmail.com>
2024-12-14 00:02:17 +0530

    feat: enhance VerifyForm layout and styling; add image to verify page for improved visual appeal

--- apps/client/app/(auth)/sign-in/page.tsx
+++ apps/client/app/(auth)/sign-in/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
   return (
     <section className="w-full max-w-screen-2xl h-screen overflow-y-hidden mx-auto px-5 md:px-14 flex items-center justify-center">
       <div className="md:w-[50%] w-full h-full flex items-center justify-center px-10">
-        <div className="w-full px-5 border-none shadow-none">
+        <div className="w-full px-5">
           <h1 className="text-3xl font-bold">{!next ? "Login" : "Verify"}</h1>
           <div className="w-full py-10">
             {!next ? <SignInForm setNext={setNext} /> : <VerifyForm />}
--- apps/client/app/(auth)/verify/page.tsx
+++ apps/client/app/(auth)/verify/page.tsx
@@ -1,15 +1,28 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import React from "react";
 import VerifyForm from "../_components/VerifyForm";
+import Image from "next/image";
 
 const Page = () => {
   return (
-    <Card className="w-96 h-[550px] px-5 py-8">
-      <CardTitle>Verify</CardTitle>
-      <CardContent className="w-full p-0 py-10">
-        <VerifyForm />
-      </CardContent>
-    </Card>
+    <section className="w-full max-w-screen-2xl h-screen overflow-y-hidden mx-auto px-5 md:px-14 flex items-center justify-center">
+      <div className="md:w-[50%] w-full h-full flex items-center justify-center px-10">
+        <div className="w-full px-5">
+          <h1 className="text-3xl font-bold">Verify</h1>
+          <div className="w-full py-10">
+            <VerifyForm />
+          </div>
+        </div>
+      </div>
+      <div className="w-[50%] hidden md:flex items-center justify-center">
+        <Image
+          src={"/auth/auth.png"}
+          alt={"sign-in"}
+          width={800}
+          height={800}
+          className="object-cover h-full w-full shrink-0"
+        />
+      </div>
+    </section>
   );
 };
 

0ea292a2eeddf9723e825f26f50430773902bc88
gourab8389 <deyg6988@gmail.com>
2024-12-13 23:31:39 +0530

    feat: refactor SignUp page layout for improved responsiveness and styling; add image for enhanced visual appeal

--- apps/client/app/(auth)/sign-up/page.tsx
+++ apps/client/app/(auth)/sign-up/page.tsx
@@ -2,19 +2,32 @@
 
 import React, { useState } from "react";
 import SignUpForm from "../_components/SignUpForm";
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import VerifyForm from "../_components/VerifyForm";
+import Image from "next/image";
 
 const Page = () => {
   const [next, setNext] = useState(false);
 
   return (
-    <Card className="w-96 h-[550px] px-5 py-8">
-      <CardTitle>{!next ? "Signup" : "Verify"}</CardTitle>
-      <CardContent className="w-full p-0 py-10">
-        {!next ? <SignUpForm setNext={setNext} /> : <VerifyForm />}
-      </CardContent>
-    </Card>
+    <section className="w-full max-w-screen-2xl min-h-screen mx-auto px-5 md:px-14 flex items-center justify-center">
+      <div className="md:w-[50%] w-full h-full flex items-center justify-center px-10">
+        <div className="w-full px-5 border-none shadow-none">
+          <h1 className="text-3xl font-bold">{!next ? "Signup" : "Verify"}</h1>
+          <div className="w-full py-10">
+            {!next ? <SignUpForm setNext={setNext} /> : <VerifyForm />}
+          </div>
+        </div>
+      </div>
+      <div className="w-[50%] hidden md:flex items-center justify-center">
+        <Image
+          src={"/auth/auth.png"}
+          alt={"sign-in"}
+          width={800}
+          height={800}
+          className="object-cover h-full w-full shrink-0"
+        />
+      </div>
+    </section>
   );
 };
 

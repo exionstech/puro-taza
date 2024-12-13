111374856aae01acd7d95af620e48111a2c905a7
gourab8389 <deyg6988@gmail.com>
2024-12-14 00:37:33 +0530

    feat: update layout and styling for SignIn, SignUp, and Verify pages; enhance footer and item card components for improved user experience

--- apps/client/app/(routes)/(public)/_components/footer.tsx
+++ apps/client/app/(routes)/(public)/_components/footer.tsx
@@ -25,8 +25,8 @@ const footerItemsCategories = [
 
 const FooterSection = () => {
   return (
-    <section className="w-full max-w-screen-2xl px-[1rem] md:px-14 mx-auto flex flex-col md:flex-row items-start gap-10 mt-10 md:mt-20">
-      <div className="md:w-[60%] w-full flex gap-6 lg:justify-between">
+    <section className="w-full border-t max-w-screen-2xl px-[1rem] md:px-14 mx-auto flex flex-col md:flex-row items-start gap-10 mt-10 md:mt-20">
+      <div className="md:w-[60%] w-full mt-5 flex gap-6 lg:justify-between">
         <div className="w-[40%] md:w-1/2 flex flex-col gap-4">
           <div className="w-full">
             <h1 className="text-2xl text-customBlack font-medium">
@@ -92,7 +92,7 @@ const FooterSection = () => {
         </div>
       </div>
 
-      <div className="md:w-[40%] w-full flex items-center justify-center">
+      <div className="md:w-[40%] w-full flex items-center justify-center mt-5">
         <div className="w-full aspect-video rounded-lg overflow-hidden bg-white border">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.501244011331!2d88.48770200862216!3d22.56034883333806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a020b007af9ca31%3A0xa3a41f4146303146!2sUEM!5e0!3m2!1sen!2sin!4v1731930397063!5m2!1sen!2sin"
--- apps/client/app/(routes)/(public)/_components/sub-components/best-sellers/item-card.tsx
+++ apps/client/app/(routes)/(public)/_components/sub-components/best-sellers/item-card.tsx
@@ -18,12 +18,8 @@ const ItemCard = ({
   const discountedPrice = itemPrice - (itemPrice * discount) / 100;
 
   return (
-    <div className="relative">
       <Card
-        className="px-5 py-4 items-center justify-center flex flex-col md:gap-2 w-full 
-                   transition-all duration-500 ease-in-out 
-                   hover:scale-100 hover:shadow-sm 
-                   cursor-pointer peer"
+        className="px-5 py-4 items-center justify-center flex flex-col md:gap-2 w-full"
       >
         <div className="w-full flex items-center justify-center overflow-hidden">
           <Image
@@ -31,14 +27,12 @@ const ItemCard = ({
             alt={itemName}
             height={250}
             width={250}
-            className="shrink-0 transition-transform duration-300 
-                       peer-hover:scale-110"
+            className="shrink-0"
           />
         </div>
         <div className="flex flex-col w-full">
           <h1
-            className="text-customBlack text-xl font-medium transition-colors duration-300 
-                         peer-hover:text-violet"
+            className="text-customBlack text-xl font-medium"
           >
             {itemName}
           </h1>
@@ -46,8 +40,7 @@ const ItemCard = ({
             <div className="flex flex-col">
               <h1 className="text-lg font-semibold">
                 <span
-                  className="text-sm font-medium text-red-500 transition-transform duration-300 
-                                peer-hover:scale-110 mr-2"
+                  className="text-sm font-medium text-red-500 mr-2"
                 >
                   -{discount}%
                 </span>{" "}
@@ -58,9 +51,7 @@ const ItemCard = ({
             <div className="flex items-center justify-center">
               <Button
                 variant={"outline"}
-                className="border px-8 border-violet text-violet 
-                           transition-all duration-300 
-                           peer-hover:bg-violet peer-hover:text-white"
+                className="border px-8 border-violet text-violet"
               >
                 ADD
               </Button>
@@ -68,7 +59,6 @@ const ItemCard = ({
           </div>
         </div>
       </Card>
-    </div>
   );
 };
 

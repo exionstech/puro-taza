437375ae0453e1f58f5ae650d85296afa04ba7d9
Atmajo <atmajoc@gmail.com>
2024-12-16 13:52:33 +0530

    feat: enhance user access control and simplify webhook user creation logic

--- apps/admin/app/api/webhooks/route.ts
+++ apps/admin/app/api/webhooks/route.ts
@@ -1,12 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser } from "@/actions/user";
 import { UserType } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
-  const client = await clerkClient();
   const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET!;
 
   if (!WEBHOOK_SECRET) {
@@ -68,11 +67,9 @@ export async function POST(req: NextRequest) {
       role: "USER" as UserType,
     };
 
-    await client.users.updateUser(clerkId, {
-      externalId: userData.role,
-    });
-
     const newUser = await createUser(userData);
+
+    console.log("New user created:", newUser);
 
     return NextResponse.json({ message: "New user created", user: newUser });
   }
